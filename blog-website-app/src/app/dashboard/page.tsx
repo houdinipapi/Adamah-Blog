0030a447bb55fb41e5d97831584968e2868acca9
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-06 15:07:12 +0300

    feat: Update dashboard page with background image and content
    The dashboard page has been updated to include a background image and additional content. This enhances the visual appeal of the page and provides more information to the users. The changes align with the established convention of using the "feat" type for new feature additions.

--- blog-website-app/src/app/dashboard/page.tsx
+++ blog-website-app/src/app/dashboard/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const Dashboard = () => {
   return (
-    <div>
-        <h1 className="font-bold text-center mt-10">Dashboard</h1>
+    <div className="relative min-h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+
+      <div className="absolute inset-0 w-full h-full overflow-hidden">
+        <Image
+          src="/images/dash1.jpg"
+          alt="Dashboard Image"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          className="opacity-50"
+        />
+      </div>
+
+      <div className="relative z-10 bg-white p-6 rounded-lg shadow-lg">
+        <div>
+          <h1 className="font-bold text-center text-3xl mt-10">Dashboard</h1>
+        </div>
+        <div className="mt-4">
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolore dicta reiciendis ab vel dolor aliquid, cumque culpa assumenda expedita aliquam ducimus temporibus nihil deleniti odio voluptas rerum, non velit dignissimos esse deserunt. Perspiciatis repellat nobis tenetur cum nesciunt officia possimus, corrupti sequi doloremque nam consequuntur eveniet omnis, nemo molestias.
+          </p>
+        </div>
+        
+      </div>
+        
+
     </div>
   )
 }
--- blog-website-app/src/components/footer.tsx
+++ blog-website-app/src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4 fixed w-full bottom-0">
+    <footer className="bg-gray-800 text-white py-4 w-full bottom-0">
         <div className="container mx-auto px-4 text-center">
             <p>
                 © {new Date().getFullYear()} Adamah. All rights reserved.
