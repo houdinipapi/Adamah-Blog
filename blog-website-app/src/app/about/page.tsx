800d52453efee707d48b047fbf536fe18609349e
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-08 22:03:58 +0300

    feat: Update about page with background image and content
    The about page has been updated to include a background image and additional content. This enhances the visual appeal of the page and provides more information to the users. The changes align with the established convention of using the "feat" type for new feature additions.

--- blog-website-app/src/app/about/page.tsx
+++ blog-website-app/src/app/about/page.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import Image from 'next/image';
 
 export default function About() {
   return (
-    <div>
-      <h1 className="font-bold text-center mt-10">About</h1>
+    <div className="relative min-h-screen bg-gray-50 flex items-center justify-center py-10 px-4 sm:px-6 lg:px-8">
+
+      <div className="absolute inset-0 w-full h-full overflow-hidden">
+        <Image
+          src="/images/dash1.jpg"
+          alt="Dashboard Image"
+          layout="fill"
+          objectFit="cover"
+          className="opacity-70"
+        />
+      </div>
+
+      <div className="relative">
+        <div>
+          <h1 className="font-bold text-center text-3xl">About</h1>
+        </div>
+        <div className="my-4 columns-2">
+
+          <p className="text-lg shadow-lg rounded-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolore dicta reiciendis ab vel dolor aliquid, cumque culpa assumenda expedita aliquam ducimus temporibus nihil deleniti odio voluptas rerum, non velit dignissimos esse deserunt. Perspiciatis repellat nobis tenetur cum nesciunt officia possimus, corrupti sequi doloremque nam consequuntur eveniet omnis, nemo molestias.
+          </p>
+
+          
+
+          
+        </div>
+        
+      </div>
+        
+
     </div>
   )
 }
--- blog-website-app/src/app/page.tsx
+++ blog-website-app/src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div>
       <h1 className="font-bold text-center mt-10">Home Page</h1>
+
+      <div>
+
+      </div>
     </div>
   );
 }
